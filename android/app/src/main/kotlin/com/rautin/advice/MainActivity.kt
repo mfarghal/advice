16fb0fec498ca9501f8c20bf9fcbdd493f311828
mfarghal <m.farghal.a@gmail.com>
2023-07-28 21:22:19 +0300

    init

---
+++ android/app/src/main/kotlin/com/rautin/advice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rautin.advice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'application/core/theme_service.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const Placeholder(),
+      );
+    });
+  }
+}
